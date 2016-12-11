1d212f5f914043fcbf694313f2ff2f915e5fa1fe
Abhishek R <abishek.ravichandran@gmail.com>
2016-12-10 21:19:55 -0500

    changed facebook redirection

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -49,14 +49,14 @@ module.exports = function (app, model) {
     app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
     app.get('/auth/google/callback',
         passport.authenticate('google', {
-            successRedirect: '/project/#/home',
-            failureRedirect: '/project/#/login'
+            successRedirect: '/#/',
+            failureRedirect: '/#/login'
         }));
     app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
-            successRedirect: '/project/#/home',
-            failureRedirect: '/project/#/login'
+            successRedirect: '/#/',
+            failureRedirect: '/#/login'
         }));
 
 
